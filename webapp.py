b168b07fee2234470e36796dec80b9bb4dbd9c0b
mshafii <m571s582@ku.edu>
2015-02-18 15:07:23 -0600

    added datastream page and table

--- webapp.py
+++ webapp.py
@@ -2,10 +2,16 @@ import flask
 app = flask.Flask(__name__)
 
 @app.route('/')
-def not_hello_world():
-    author = "Nick"
-    name = "Home?"
+def home():
+    author = "Nick, Monica"
+    name = "Home"
     return flask.render_template('index.html', author=author, name=name)
+
+@app.route('/datastream')
+def datastream():
+    author = "Nick, Monica"
+    name = "Datastream"
+    return flask.render_template('datastream.html', author=author, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
