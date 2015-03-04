eab13447ac857b09fb2a917133b6f2dd68b235f5
mshafii <m571s582@ku.edu>
2015-03-04 15:14:44 -0600

    Created fake JSON object to test printing/parsing data to html table

--- webapp.py
+++ webapp.py
@@ -2,6 +2,9 @@ import flask
 import time
 import string
 import random
+from flask import jsonify
+from collections import deque
+
 app = flask.Flask(__name__)
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
@@ -18,19 +21,25 @@ def home():
     name = "Home"
     return flask.render_template('index.html', author=author, name=name)
 
+@app.route('/deletelater')
+def fakeData():
+    dataDic = [ { "time": 123123123123, "departure_time": 123123123123, "headsign": "to Daisy Hill", "stop": { "id": "idStop", "code": "codeStop", "name": "nameStop1" }, "route": { "agency": { "id": "idAgency", "name": "nameAgency1", "url": "urlAgency" }, "id": "idRoute", "short_name": "nameRoute1" } }, 
+                { "time": 123123123123, "departure_time": 123123123123, "headsign": "to Daisy Hill", "stop": { "id": "idStop", "code": "codeStop", "name": "nameStop2" }, "route": { "agency": { "id": "idAgency", "name": "nameAgency2", "url": "urlAgency" }, "id": "idRoute", "short_name": "nameRoute2" } }, 
+                { "time": 123123123123, "departure_time": 123123123123, "headsign": "to Daisy Hill", "stop": { "id": "idStop", "code": "codeStop", "name": "nameStop3" }, "route": { "agency": { "id": "idAgency", "name": "nameAgency3", "url": "urlAgency" }, "id": "idRoute", "short_name": "nameRoute3" } }, 
+                { "time": 123123123123, "departure_time": 123123123123, "headsign": "to Daisy Hill", "stop": { "id": "idStop", "code": "codeStop", "name": "nameStop4" }, "route": { "agency": { "id": "idAgency", "name": "nameAgency4", "url": "urlAgency" }, "id": "idRoute", "short_name": "nameRoute4" } }, 
+                { "time": 123123123123, "departure_time": 123123123123, "headsign": "to Daisy Hill", "stop": { "id": "idStop", "code": "codeStop", "name": "nameStop5" }, "route": { "agency": { "id": "idAgency", "name": "nameAgency5", "url": "urlAgency" }, "id": "idRoute", "short_name": "nameRoute5" } } ]
+    return flask.json.jsonify(arrivals = dataDic)
+
+
 @app.route('/datastream')
 def datastream():
     author = "Nick, Monica"
     name = "Datastream"
 
-    while True:
-	s1 = Struct()
-        theTime = time.asctime( time.localtime( time.time() ) )
-        s1.route = id_generator()
-        s1.city = id_generator()
-        s1.onTime = id_generator()
-        return flask.render_template('datastream.html', author=author, name=name, theTime=theTime, s1=s1)
-        time.sleep(10)
+    theTime = time.asctime( time.localtime( time.time() ) )
+        
+    return flask.render_template('datastream.html', author=author, name=name, theTime=theTime)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
