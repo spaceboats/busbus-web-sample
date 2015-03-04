220b98f91ad78c67fc51a36ff9c5af3828fcfd17
mshafii <m571s582@ku.edu>
2015-03-04 11:44:28 -0600

    Updated html format in datastream. Experimenting with adding random data and system time.

--- webapp.py
+++ webapp.py
@@ -1,5 +1,16 @@
 import flask
+import time
+import string
+import random
 app = flask.Flask(__name__)
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+class Struct:
+    route = ""
+    city = ""
+    onTime = ""
 
 @app.route('/')
 def home():
@@ -11,7 +22,15 @@ def home():
 def datastream():
     author = "Nick, Monica"
     name = "Datastream"
-    return flask.render_template('datastream.html', author=author, name=name)
+
+    while True:
+	s1 = Struct()
+        theTime = time.asctime( time.localtime( time.time() ) )
+        s1.route = id_generator()
+        s1.city = id_generator()
+        s1.onTime = id_generator()
+        return flask.render_template('datastream.html', author=author, name=name, theTime=theTime, s1=s1)
+        time.sleep(10)
 
 if __name__ == '__main__':
     app.run(debug=True)
